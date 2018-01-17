71be088e660de080fa5ff6a9aa264b0bba8f577d
jerry <1083928496@qq.com>
2018-01-17 17:16:18 +0800

    thymeleaf layout

--- src/main/java/com/pikapika/app/mapper/CharactorMapper.java
+++ src/main/java/com/pikapika/app/mapper/CharactorMapper.java
@@ -5,9 +5,20 @@ import java.util.List;
 import com.pikapika.app.entity.CharactorEntity;
 
 public interface CharactorMapper {
-	public List<CharactorEntity> getAllCharactors(); 
-	
-	public CharactorEntity getCharactorById(String cId);
-	
-	public void updateCharactor(CharactorEntity charactorEntity);
+	/**
+	 * 查询船员
+	 * @return 船员列表
+	 */
+	List<CharactorEntity> getAllCharactors(); 
+	/**
+	 * 查询船员通过船员Id
+	 * @param cId
+	 * @return 船员
+	 */
+	CharactorEntity getCharactorById(String cId);
+	/**
+	 * 更新船员信息
+	 * @param charactorEntity 船员信息
+	 */
+	void updateCharactor(CharactorEntity charactorEntity);
 }
